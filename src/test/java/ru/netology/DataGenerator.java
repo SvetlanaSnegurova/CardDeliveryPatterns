257318abc14b0ed953071699c815af81182e1a43
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-05-09 22:47:53 +0300

    Added TEST

---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,61 @@
+package ru.netology;
+
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+
+    private DataGenerator() {
+    }
+
+    public static @NotNull String generateDate(int days) {
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String generateCity() {
+        // с помощью Faker, либо используя массив валидных городов и класс Random
+        var cites = new String[]{"Архангельск", "Астрахань", "Барнаул", "Владикавказ",
+                "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Махачкала",
+                "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск",
+                "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва",
+                "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону"};
+        return cites[new Random().nextInt(cites.length)];
+    }
+
+    public static @NotNull String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        @Contract("_ -> new")
+        public static @NotNull UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
+
